39de72cc5b1d33900fa685d4e485edb7e0c9f5f9
Alexis <omaroquendo221@gmail.com>
2020-07-21 18:04:51 -0500

    Primeros pasos 1

---
+++ Variables,Constantes.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+
+    val NombreMascota = "El firulais"
+    var edadMascota = 18
+    edadMascota = 15
+
+    println("El nombre de la mascota es " +NombreMascota+ " Y la edad es " +edadMascota)
+}
