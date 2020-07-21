d42fb74d62fb32da9f897e8c421b279c0191b174
Alexis <omaroquendo221@gmail.com>
2020-07-21 18:54:03 -0500

    Primeros pasos 3

---
+++ Arreglos.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var i = 1
+    var arreglo:Array<String> = arrayOf("Mazda","Toyota","Audi")
+    println(arreglo[2])
+    
+    var arre = intArrayOf(1, 2, 3, 4, 5)
+    println(arre[1])
+    var arreglo2:Array<Int> = arrayOf(1,5,3)
+    println(arreglo2[1])
+
+}
