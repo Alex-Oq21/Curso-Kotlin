7adb14c27734bc7ab2443388e89c2d3f38eb8f08
Alexis <omaroquendo221@gmail.com>
2020-07-22 18:02:31 -0500

    Primeros pasos

---
+++ Funciones.kt
@@ -0,0 +1,12 @@
+//Funciones
+fun main(args: Array<String>) {
+    var resultado = suma(5,5)
+    //print(resultado)
+    
+ val cosenoUno = Math.cos(1.0) 
+    print(cosenoUno)
+
+}
+fun suma(a:Int, b:Int):Int{
+    return a+b
+}
